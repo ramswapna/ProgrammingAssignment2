993f6dd4a8fe2f62ec06f97a77b705644eda7a81
ramswapna <swapnahdp9@gmail.com>
2018-04-14 11:16:09 -0400

    Added logic to cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,21 @@
 
 ## Write a short comment describing this function
 
+## The below function is used to set and get the matrix ,inverse of a matrix values.
+
 makeCacheMatrix <- function(x = matrix()) {
+  m<- NULL
+  set <- function(y){
+    x<<- y
+    m<<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  getmean <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
@@ -11,5 +25,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+	if(!is.null(inverse)){
+	 print("Getting inverse of a matrix data from cache")
+	 return(inverse)
+	}
+    data <- x$get()
+    inverse <- solve(data,...)
+    x$setInverse(inverse)
+    inverse
 }
